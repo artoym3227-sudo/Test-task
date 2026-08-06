80e7832998d86be5f8bc35974605712f475efa4f
artoym3227-sudo <artoym3227@gmail.com>
2026-08-07 02:59:33 +0500

    main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import '@/styles/_resets.scss'
 import '@/styles/main.scss'
 
-
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(Toast, {
+        position: 'top-right',
+        timeout: 4000,
+    })
+    .mount('#app')
